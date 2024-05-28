95d2ed27d8ba534f1d2697e084f41214d1231f92
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-05-28 20:05:18 +0200

    set style and add documentation to the Controller classes

--- PLM/src/plm/controller/DocumentController.java
+++ PLM/src/plm/controller/DocumentController.java
@@ -4,42 +4,100 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import plm.services.DocumentService;
 
+/**
+ * Controller for managing Document entities in the Product Lifecycle Management (PLM) system.
+ */
 public class DocumentController {
-	
+
     @Autowired
     private DocumentService documentService;
-	
-	@GetMapping(value = "/Document/reserve")
-    public void reserve(@RequestHeader("userId") String userId, @RequestParam("reference") String reference, @RequestParam("version") String version, @RequestParam("iteration") int iteration) {
-		
-		documentService.reserve(userId, reference, version, iteration);
-	}
 
-	@GetMapping(value = "/Document/update")
-	public void update(@RequestHeader("userId") String userId, @RequestParam("reference") String reference, @RequestParam("version") String version, @RequestParam("iteration") int iteration, @RequestParam("documentAttribute1") String documentAttribute1, @RequestParam("documentAttribute2") String documentAttribute2) {
-		//
-		documentService.update(userId, reference, version, iteration, documentAttribute1, documentAttribute2);
-	}
-	
-	@GetMapping(value = "/Document/free")
-	public void free(@RequestHeader("userId") String userId, @RequestParam("reference") String reference, @RequestParam("version") String version, @RequestParam("iteration") int iteration) {
-		
-		documentService.free(userId, reference, version, iteration);
-	}
-	
-	@GetMapping(value = "/Document/free")
-	public void setState(@RequestHeader("userId") String userId, @RequestParam("reference") String reference, @RequestParam("version") String version, @RequestParam("iteration") int iteration, @RequestParam("state") String state) {
-		
-		documentService.setState(userId, reference, version, iteration, state);
-	}
-	
-	@GetMapping(value = "/Document/revise")
-	public void revise(@RequestHeader("userId") String userId, @RequestParam("reference") String reference, @RequestParam("version") String version, @RequestParam("iteration") int iteration) {
-		
-		documentService.revise(userId, reference, version, iteration);
-	}
-	
+    /**
+     * Reserves a Document entity.
+     *
+     * @param userId    The user ID making the request.
+     * @param reference The reference of the document.
+     * @param version   The version of the document.
+     * @param iteration The iteration of the document.
+     */
+    @GetMapping(value = "/Document/reserve")
+    public void reserve(@RequestHeader("userId") String userId,
+                        @RequestParam("reference") String reference,
+                        @RequestParam("version") String version,
+                        @RequestParam("iteration") int iteration) {
+        documentService.reserve(userId, reference, version, iteration);
+    }
+
+    /**
+     * Updates a Document entity.
+     *
+     * @param userId             The user ID making the request.
+     * @param reference          The reference of the document.
+     * @param version            The version of the document.
+     * @param iteration          The iteration of the document.
+     * @param documentAttribute1 The first attribute to update.
+     * @param documentAttribute2 The second attribute to update.
+     */
+    @GetMapping(value = "/Document/update")
+    public void update(@RequestHeader("userId") String userId,
+                       @RequestParam("reference") String reference,
+                       @RequestParam("version") String version,
+                       @RequestParam("iteration") int iteration,
+                       @RequestParam("documentAttribute1") String documentAttribute1,
+                       @RequestParam("documentAttribute2") String documentAttribute2) {
+        documentService.update(userId, reference, version, iteration,
+                documentAttribute1, documentAttribute2);
+    }
+
+    /**
+     * Frees a reserved Document entity.
+     *
+     * @param userId    The user ID making the request.
+     * @param reference The reference of the document.
+     * @param version   The version of the document.
+     * @param iteration The iteration of the document.
+     */
+    @GetMapping(value = "/Document/free")
+    public void free(@RequestHeader("userId") String userId,
+                     @RequestParam("reference") String reference,
+                     @RequestParam("version") String version,
+                     @RequestParam("iteration") int iteration) {
+        documentService.free(userId, reference, version, iteration);
+    }
+
+    /**
+     * Sets the state of a Document entity.
+     *
+     * @param userId    The user ID making the request.
+     * @param reference The reference of the document.
+     * @param version   The version of the document.
+     * @param iteration The iteration of the document.
+     * @param state     The new state of the document.
+     */
+    @GetMapping(value = "/Document/free")
+    public void setState(@RequestHeader("userId") String userId,
+                         @RequestParam("reference") String reference,
+                         @RequestParam("version") String version,
+                         @RequestParam("iteration") int iteration,
+                         @RequestParam("state") String state) {
+        documentService.setState(userId, reference, version, iteration, state);
+    }
+
+    /**
+     * Revises a Document entity.
+     *
+     * @param userId    The user ID making the request.
+     * @param reference The reference of the document.
+     * @param version   The version of the document.
+     * @param iteration The iteration of the document.
+     */
+    @GetMapping(value = "/Document/revise")
+    public void revise(@RequestHeader("userId") String userId,
+                       @RequestParam("reference") String reference,
+                       @RequestParam("version") String version,
+                       @RequestParam("iteration") int iteration) {
+        documentService.revise(userId, reference, version, iteration);
+    }
 }
--- PLM/src/plm/controller/PartController.java
+++ PLM/src/plm/controller/PartController.java
@@ -4,42 +4,100 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import plm.services.PartService;
 
+/**
+ * Controller for managing Part entities in the Product Lifecycle Management (PLM) system.
+ */
 public class PartController {
-	
+
     @Autowired
     private PartService partService;
-	
-	@GetMapping(value = "/Part/reserve")
-    public void reserve(@RequestHeader("userId") String userId, @RequestParam("reference") String reference, @RequestParam("version") String version, @RequestParam("iteration") int iteration) {
-		
-		partService.reserve(userId, reference, version, iteration);
-	}
 
-	@GetMapping(value = "/Part/update")
-	public void update(@RequestHeader("userId") String userId, @RequestParam("reference") String reference, @RequestParam("version") String version, @RequestParam("iteration") int iteration, @RequestParam("partAttribute1") String partAttribute1, @RequestParam("partAttribute2") String partAttribute2) {
-		//
-		partService.update(userId, reference, version, iteration, partAttribute1, partAttribute2);
-	}
-	
-	@GetMapping(value = "/Part/free")
-	public void free(@RequestHeader("userId") String userId, @RequestParam("reference") String reference, @RequestParam("version") String version, @RequestParam("iteration") int iteration) {
-		
-		partService.free(userId, reference, version, iteration);
-	}
-	
-	@GetMapping(value = "/Part/free")
-	public void setState(@RequestHeader("userId") String userId, @RequestParam("reference") String reference, @RequestParam("version") String version, @RequestParam("iteration") int iteration, @RequestParam("state") String state) {
-		
-		partService.setState(userId, reference, version, iteration, state);
-	}
-	
-	@GetMapping(value = "/Part/revise")
-	public void revise(@RequestHeader("userId") String userId, @RequestParam("reference") String reference, @RequestParam("version") String version, @RequestParam("iteration") int iteration) {
-		
-		partService.revise(userId, reference, version, iteration);
-	}
-	
+    /**
+     * Reserves a Part entity.
+     *
+     * @param userId    The user ID making the request.
+     * @param reference The reference of the part.
+     * @param version   The version of the part.
+     * @param iteration The iteration of the part.
+     */
+    @GetMapping(value = "/Part/reserve")
+    public void reserve(@RequestHeader("userId") String userId,
+                        @RequestParam("reference") String reference,
+                        @RequestParam("version") String version,
+                        @RequestParam("iteration") int iteration) {
+        partService.reserve(userId, reference, version, iteration);
+    }
+
+    /**
+     * Updates a Part entity.
+     *
+     * @param userId         The user ID making the request.
+     * @param reference      The reference of the part.
+     * @param version        The version of the part.
+     * @param iteration      The iteration of the part.
+     * @param partAttribute1 The first attribute to update.
+     * @param partAttribute2 The second attribute to update.
+     */
+    @GetMapping(value = "/Part/update")
+    public void update(@RequestHeader("userId") String userId,
+                       @RequestParam("reference") String reference,
+                       @RequestParam("version") String version,
+                       @RequestParam("iteration") int iteration,
+                       @RequestParam("partAttribute1") String partAttribute1,
+                       @RequestParam("partAttribute2") String partAttribute2) {
+        partService.update(userId, reference, version, iteration,
+                partAttribute1, partAttribute2);
+    }
+
+    /**
+     * Frees a reserved Part entity.
+     *
+     * @param userId    The user ID making the request.
+     * @param reference The reference of the part.
+     * @param version   The version of the part.
+     * @param iteration The iteration of the part.
+     */
+    @GetMapping(value = "/Part/free")
+    public void free(@RequestHeader("userId") String userId,
+                     @RequestParam("reference") String reference,
+                     @RequestParam("version") String version,
+                     @RequestParam("iteration") int iteration) {
+        partService.free(userId, reference, version, iteration);
+    }
+
+    /**
+     * Sets the state of a Part entity.
+     *
+     * @param userId    The user ID making the request.
+     * @param reference The reference of the part.
+     * @param version   The version of the part.
+     * @param iteration The iteration of the part.
+     * @param state     The new state of the part.
+     */
+    @GetMapping(value = "/Part/free")
+    public void setState(@RequestHeader("userId") String userId,
+                         @RequestParam("reference") String reference,
+                         @RequestParam("version") String version,
+                         @RequestParam("iteration") int iteration,
+                         @RequestParam("state") String state) {
+        partService.setState(userId, reference, version, iteration, state);
+    }
+
+    /**
+     * Revises a Part entity.
+     *
+     * @param userId    The user ID making the request.
+     * @param reference The reference of the part.
+     * @param version   The version of the part.
+     * @param iteration The iteration of the part.
+     */
+    @GetMapping(value = "/Part/revise")
+    public void revise(@RequestHeader("userId") String userId,
+                       @RequestParam("reference") String reference,
+                       @RequestParam("version") String version,
+                       @RequestParam("iteration") int iteration) {
+        partService.revise(userId, reference, version, iteration);
+    }
 }
