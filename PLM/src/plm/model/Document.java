84ddf71944482a8d66f5ca5ed49fd338929fdefb
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-05-28 15:21:14 +0200

    Document inherits from Part

--- PLM/src/plm/model/Document.java
+++ PLM/src/plm/model/Document.java
@@ -1,151 +1,154 @@
 package plm.model;
 
 import java.util.Objects;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
-import javax.persistence.ManyToOne;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Entity class representing a Document in the Product Lifecycle Management (PLM) system.
+ */
 @Entity
 @IdClass(plm.model.Document.DocumentPK.class)
-public class Document {
-	
-	@Id
-	private String reference;
-	@Id
-	private String version;
-	@Id
-	private int iteration;
-	
-	@Column
-	private boolean reserved;
-	@Column
-	private String  reservedBy;
+public class Document extends Part {
 
-	@ManyToOne
-	private LifeCycleTemplate lifeCycleTemplate;
-	@Column
-	private String lifeCycleState;
-	
-	@ManyToOne
-	private VersionSchema versionSchema;
-	
-	@Column
-	private String documentAttribute1;
-	@Column
-	private String documentAttribute2;
-	
-	public Document() 
-	{
-		
-	}
-	
-	public Document(String reference, String version, int iteration) {
-		this.reference = reference;
-		this.version = version;
-		this.iteration = iteration;
-	}
+    private static final Logger logger = LoggerFactory.getLogger(Document.class);
 
-	public String getReference() {
-		return reference;
-	}
+    @Column
+    private String documentAttribute1;
 
-	public String getVersion() {
-		return version;
-	}
+    @Column
+    private String documentAttribute2;
 
-	public int getIteration() {
-		return iteration;
-	}
+    // Constructors
 
-	public boolean isReserved() 
-	{
-		return reserved;
-	}
+    /**
+     * Default constructor for Java Persistance API.
+     */
+    public Document() {
+        // Default constructor
+    }
 
-	public String getReservedBy() {
-		return reservedBy;
-	}
+    /**
+     * Constructor to initialize the composite primary key fields.
+     *
+     * @param reference The reference of the document.
+     * @param version   The version of the document.
+     * @param iteration The iteration of the document.
+     */
+    public Document(String reference, String version, int iteration) {
+        super(reference, version, iteration);
+    }
 
-	public LifeCycleTemplate getLifeCycleTemplate() {
-		return lifeCycleTemplate;
-	}
+    // Getters
 
-	public String getLifeCycleState() {
-		return lifeCycleState;
-	}
+    /**
+     * Gets the first document attribute.
+     *
+     * @return The first document attribute.
+     */
+    public String getDocumentAttribute1() {
+        return documentAttribute1;
+    }
 
-	public VersionSchema getVersionSchema() {
-		return versionSchema;
-	}
+    /**
+     * Gets the second document attribute.
+     *
+     * @return The second document attribute.
+     */
+    public String getDocumentAttribute2() {
+        return documentAttribute2;
+    }
 
-	public String getDocumentAttribute1() {
-		return documentAttribute1;
-	}
+    // Setters
 
-	public String getDocumentAttribute2() {
-		return documentAttribute2;
-	}
+    /**
+     * Sets the first document attribute.
+     *
+     * @param documentAttribute1 The new first document attribute.
+     * @return The current Document instance (for chaining).
+     */
+    public Document setDocumentAttribute1(String documentAttribute1) {
+        if (documentAttribute1 == null || documentAttribute1.trim().isEmpty()) {
+            logger.error("Attempted to set invalid documentAttribute1: {}", documentAttribute1);
+            throw new IllegalArgumentException("Document attribute 1 cannot be blank");
+        }
+        this.documentAttribute1 = documentAttribute1;
+        return this;
+    }
 
-	public void setReserved(boolean reserved) {
-		this.reserved = reserved;
-	}
+    /**
+     * Sets the second document attribute.
+     *
+     * @param documentAttribute2 The new second document attribute.
+     * @return The current Document instance (for chaining).
+     */
+    public Document setDocumentAttribute2(String documentAttribute2) {
+        if (documentAttribute2 == null || documentAttribute2.trim().isEmpty()) {
+            logger.error("Attempted to set invalid documentAttribute2: {}", documentAttribute2);
+            throw new IllegalArgumentException("Document attribute 2 cannot be blank");
+        }
+        this.documentAttribute2 = documentAttribute2;
+        return this;
+    }
 
-	public void setReservedBy(String reservedBy) {
-		this.reservedBy = reservedBy;
-	}
+    /**
+     * Composite primary key class for the Document entity.
+     */
+    public static class DocumentPK extends Part.PartPK {
+        /**
+         * Default constructor for Java Persistance API.
+         */
+        public DocumentPK() {
+            // Default constructor
+        }
 
-	public void setLifeCycleTemplate(LifeCycleTemplate lifeCycleTemplate) {
-		this.lifeCycleTemplate = lifeCycleTemplate;
-	}
+        /**
+         * Constructor to initialize the composite key fields.
+         *
+         * @param reference The reference of the document.
+         * @param version   The version of the document.
+         * @param iteration The iteration of the document.
+         */
+        public DocumentPK(String reference, String version, int iteration) {
+            super(reference, version, iteration);
+        }
 
-	public void setLifeCycleState(String lifeCycleState) {
-		this.lifeCycleState = lifeCycleState;
-	}
+        /**
+         * Generates a hash code for the DocumentPK instance.
+         *
+         * @return A hash code based on the reference, version, and iteration fields.
+         */
+        @Override
+        public int hashCode() {
+            return Objects.hash(getReference(), getVersion(), getIteration());
+        }
 
-	public void setVersionSchema(VersionSchema versionSchema) {
-		this.versionSchema = versionSchema;
-	}
-
-	public void setDocumentAttribute1(String documentAttribute1) {
-		this.documentAttribute1 = documentAttribute1;
-	}
-
-	public void setDocumentAttribute2(String documentAttribute2) {
-		this.documentAttribute2 = documentAttribute2;
-	}
-	
-	public static class DocumentPK {
-		private String reference;
-		private String version;
-		private int iteration;
-		public DocumentPK() {
-			
-		}
-		public DocumentPK(String reference, String version, int iteration) {
-			this.reference = reference;
-			this.version = version;
-			this.iteration = iteration;
-		}
-		@Override
-		public int hashCode() {
-			return Objects.hash(iteration, reference, version);
-		}
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			DocumentPK other = (DocumentPK) obj;
-			return iteration == other.iteration && Objects.equals(reference, other.reference)
-					&& Objects.equals(version, other.version);
-		}
-		
-	}
-		
+        /**
+         * Checks if two DocumentPK instances are equal.
+         *
+         * @param obj The object to compare with.
+         * @return true if the objects are equal, false otherwise.
+         */
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            DocumentPK other = (DocumentPK) obj;
+            return Objects.equals(getReference(), other.getReference()) &&
+                    Objects.equals(getVersion(), other.getVersion()) &&
+                    getIteration() == other.getIteration();
+        }
+    }
 }
