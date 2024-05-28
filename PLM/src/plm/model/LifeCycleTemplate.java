3648af751b45678493a73cf42f970ca7782aa5c8
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2024-05-28 19:25:29 +0200

    add documentation to the classes

--- PLM/src/plm/model/LifeCycleTemplate.java
+++ PLM/src/plm/model/LifeCycleTemplate.java
@@ -2,27 +2,44 @@ package plm.model;
 
 import javax.persistence.Entity;
 
+/**
+ * Entity class representing the lifecycle template in the Product Lifecycle Management (PLM) system.
+ * This class is used to manage the lifecycle states of parts and documents.
+ *
+ * @Entity specifies that the class is an entity and is mapped to a database table.
+ */
 @Entity
 public class LifeCycleTemplate {
 
-	public String getInitialState() {
-		//
-		// Implementation and returned value are not relevant for this exercise
-		//
-		return null;
-	}
-	
-	public boolean isKnown(String lifeCycleState) {
-		//
-		// Implementation and returned value are not relevant for this exercise
-		//
-		return true;
-	}
-	
-	public boolean isFinal(String lifeCycleState) {
-		//
-		// Implementation and returned value are not relevant for this exercise
-		//
-		return true;
-	}
+    /**
+     * Gets the initial lifecycle state.
+     *
+     * @return The initial lifecycle state.
+     */
+    public String getInitialState() {
+        // Implementation and returned value are not relevant for this exercise
+        return null;
+    }
+
+    /**
+     * Checks if the given lifecycle state is known.
+     *
+     * @param lifeCycleState The lifecycle state to check.
+     * @return true if the lifecycle state is known, false otherwise.
+     */
+    public boolean isKnown(String lifeCycleState) {
+        // Implementation and returned value are not relevant for this exercise
+        return true;
+    }
+
+    /**
+     * Checks if the given lifecycle state is a final state.
+     *
+     * @param lifeCycleState The lifecycle state to check.
+     * @return true if the lifecycle state is a final state, false otherwise.
+     */
+    public boolean isFinal(String lifeCycleState) {
+        // Implementation and returned value are not relevant for this exercise
+        return true;
+    }
 }
--- PLM/src/plm/model/VersionSchema.java
+++ PLM/src/plm/model/VersionSchema.java
@@ -2,13 +2,23 @@ package plm.model;
 
 import javax.persistence.Entity;
 
+/**
+ * Entity class representing the version schema in the Product Lifecycle Management (PLM) system.
+ * This class is used to determine the next version label based on the current version label.
+ *
+ * @Entity specifies that the class is an entity and is mapped to a database table.
+ */
 @Entity
 public class VersionSchema {
-	
-	public String getNextVersionLabel(String currentVersionLabel) {
-		//
-		// Implementation and returned value are not relevant for this exercise
-		//
-		return null;
-	}
+
+    /**
+     * Gets the next version label based on the current version label.
+     *
+     * @param currentVersionLabel The current version label.
+     * @return The next version label.
+     */
+    public String getNextVersionLabel(String currentVersionLabel) {
+        // Implementation and returned value are not relevant for this exercise
+        return null;
+    }
 }
